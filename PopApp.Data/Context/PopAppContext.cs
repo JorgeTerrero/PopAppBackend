323d0677d82c334386594568f98bbdca219e1184
JorgeTerrero <jorgedavidterreri@gmail.com>
2021-05-02 13:25:09 -0400

    context of application

---
+++ PopApp.Data/Context/PopAppContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PopApp.Core.Entities;
+using PopApp.Data.Model.Config;
+
+namespace PopApp.Data.Context
+{
+    /// <summary>
+    /// Represent the DbContex of application.
+    /// </summary>
+    public class PopAppContext : DbContext
+    {
+        #region Ctor
+        /// <param name="options">dbcontext param.</param>
+        public PopAppContext(DbContextOptions<PopAppContext> options): base(options)
+        {
+
+        }
+        #endregion
+
+        #region OnModelCreating
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder">modelbuild param</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ContainerConfiguration());
+            modelBuilder.ApplyConfiguration(new FreightConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new VesselConfiguration());
+        }
+        #endregion
+
+        #region DbSets
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Container> Containers { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Freight> Freights { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Vessel> Vessels { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        #endregion
+         
+    }
+}
---
+++ PopApp.Data/Context/ScheduleContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PopApp.Core.Entities.schedule;
+
+namespace PopApp.Data.Context
+{
+    /// <summary>
+    /// Represent dbcontext of schedule.
+    /// </summary>
+    public class ScheduleContext: DbContext
+    {
+        #region Ctor
+        /// <param name="options">dbcontext param.</param>
+        public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options) { }
+        #endregion
+
+        #region Dbset
+        public DbSet<ScheduleVessel> ScheduleVessels { get; set; }
+        public DbSet<ScheduleContainer> ScheduleContainers { get; set; }
+        public DbSet<ScheduleFreight> ScheduleFreights { get; set; }
+        public DbSet<ScheduleProduct> ScheduleProducts { get; set; }
+        #endregion
+    }
+}
