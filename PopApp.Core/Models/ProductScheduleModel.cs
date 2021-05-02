e568f44da05245716da44d8dce0bbee2e5ddc275
JorgeTerrero <jorgedavidterreri@gmail.com>
2021-05-02 13:17:33 -0400

    model to do some process

---
+++ PopApp.Core/Models/ProductScheduleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopApp.Core.Models
+{
+    /// <summary>
+    /// Represent product schedule model.
+    /// </summary>
+    public class ProductScheduleModel
+    {
+        /// <summary>
+        /// Get or set product id.
+        /// </summary>
+        public int ProductId { get; set; }
+        /// <summary>
+        /// Get or set quantity.
+        /// </summary>
+        public decimal? Quantity { get; set; }
+    }
+}
---
+++ PopApp.Core/Models/ScheduleRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PopApp.Core.Models
+{
+    public sealed class ScheduleRequest
+    {
+        /// <summary>
+        /// Get or set vessel id.
+        /// </summary>
+        public int VesselId { get; set; }
+        /// <summary>
+        /// Get or set container id.
+        /// </summary>
+        public int ContainerId { get; set; }
+        /// <summary>
+        /// Get or set freight id.
+        /// </summary>
+        public int FreightId { get; set; }
+        /// <summary>
+        /// Get or set company id.
+        /// </summary>
+        public int CompanyId { get; set; }
+        /// <summary>
+        /// Get or set a List of product schedule model.
+        /// </summary>
+        public IList<ProductScheduleModel> Products { get; set; }
+    }
+}
