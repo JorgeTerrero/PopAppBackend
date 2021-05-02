1343b05391d3afd7319f6b2d8fc795fa291f1de2
JorgeTerrero <jorgedavidterreri@gmail.com>
2021-05-02 13:13:44 -0400

    enums to process some status.

---
+++ PopApp.Core/Enums/ContainerStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopApp.Core.Enums
+{
+    public enum ContainerStatus
+    {
+         WaitingForRevision = 1,
+         ReadyForDispatch = 2,
+         Delivered = 3
+    }
+}
---
+++ PopApp.Core/Enums/VesselStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopApp.Core.Enums
+{
+    public enum VesselStatus
+    {
+        InTransit = 1,
+        ReadyToLoadOrUnload = 2,
+        dispatched = 3
+    }
+}
