22e402393bf7a1f1bcf74b32ee3cfc537e3c7901
JorgeTerrero <jorgedavidterreri@gmail.com>
2021-05-02 14:35:15 -0400

    mapper config

---
+++ PopAppBackend/Mapper/MapperConfig.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PopApp.Core.Dtos;
+using PopApp.Core.Entities;
+
+namespace PopAppBackend.Mapper
+{
+    /// <summary>
+    /// Represent mapper config.
+    /// </summary>
+    public class MapperConfig : Profile
+    {
+        #region Ctor
+        /// <summary>
+        /// 
+        /// </summary>
+        public MapperConfig()
+        {
+            CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<Container, ContainerDto>().ReverseMap();
+            CreateMap<Document, DocumentDto>().ReverseMap();
+            CreateMap<Freight, FreightDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Vessel, VesselDto>().ReverseMap();
+        }
+        #endregion
+    }
+}
